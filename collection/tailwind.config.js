993af424a7199ef4d3e95775fab5028ea096d504
Andrew Morrison <andrewmorrison10@sky.com>
2022-03-11 16:52:10 +0000

    Update collection tailwind config

--- collection/tailwind.config.js
+++ collection/tailwind.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  enable: true,
+  mode: 'jit',
+  darkMode: false, 
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],// or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5', 
+        },
+      },
+    },
   },
   variants: {
     extend: {},
